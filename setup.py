4574fbca511c93db196ea8110240bd9d02cef2b6
Sergio Maffioletti <sergio.maffioletti@uzh.ch>
2019-02-20 14:42:44 +0100

    sergio: update version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cytobank-tools',
-    version='0.1.0',
+    version='0.1.2',
     description='Tools to handle Cytobank data',
     author='Anton Rau',
     author_email='anton.rau@uzh.ch',
